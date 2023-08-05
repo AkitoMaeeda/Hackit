2468dd279f7b2bb733808ae462a426f051bbc816
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2023-08-05 13:40:34 +0900

    newvergion1

---
+++ web/js/content.js
@@ -0,0 +1,59 @@
+const btn = document.getElementById("aaaa");
+const ulbody = document.getElementById('list');
+const closeIcons = document.querySelectorAll('.close-icon');
+const items = document.querySelectorAll('.item');
+
+function createNewLi(){
+    const newLi = document.createElement('li');
+    newLi.classList.add('item');
+
+    const newA = document.createElement('a');
+
+    const newP1 = document.createElement('p');
+    newP1.classList.add('date');
+    newP1.textContent = '2020/4/15 13:28';
+    
+    const newP2 = document.createElement('p');
+    newP2.classList.add('name');
+    newP2.textContent = '八巻　顕伍';
+
+    const newP3 = document.createElement('p');
+    newP3.classList.add('id');
+    newP3.textContent = '1318809';
+
+    const newP4 = document.createElement('p');
+    newP4.classList.add('things');
+    newP4.textContent = 'ほうき';
+
+    const newP5 = document.createElement('p');
+    newP5.classList.add('num');
+    newP5.textContent = '20';
+
+    const newP6 = document.createElement('p');
+    newP6.classList.add('user');
+    newP6.textContent = '長澤まさみ';
+
+    const newButton = document.createElement('button');
+    newButton.classList.add('category');
+
+    const newSpan = document.createElement('span');
+    newSpan.textContent = '返却';
+
+    newLi.appendChild(newA);
+    newA.appendChild(newP1);
+    newA.appendChild(newP2);
+    newA.appendChild(newP3);
+    newA.appendChild(newP4);
+    newA.appendChild(newP5);
+    newA.appendChild(newP6);
+    newA.appendChild(newButton);
+    newButton.appendChild(newSpan);
+
+    console.log(newLi);
+    return newLi;
+}
+
+// ボタンをクリックしたときの処理
+btn.addEventListener('click', () => {
+    ulbody.appendChild(createNewLi());
+});
---
+++ web/js/form.js
@@ -0,0 +1,82 @@
+const btn = document.querySelector('button');
+const tbl = document.getElementById('table-block');
+const tbdy = document.querySelector('tbody');
+const closeIcons = document.querySelectorAll('.close-icon');
+const items = document.querySelectorAll('.item');
+
+// 新しい行を作成し、作成した行を返す関数
+function createNewTr() {
+    const newTr = document.createElement('tr');
+
+    const newTd1 = document.createElement('td');
+
+    const newdiv1 = document.createElement('div');
+    newdiv1.classList.add('Form-Item');
+
+    const newp1 = document.createElement('p');
+    newp1.classList.add('Form-Item-Label');
+    newp1.textContent = '借用物：';
+
+    const newspan1 = document.createElement('span');
+    newspan1.classList.add('Form-Item-Label-Required2');
+    newspan1.textContent = '任意';
+
+    const newinput1 = document.createElement('input');
+    newinput1.type = 'text';
+    newinput1.classList.add('Form-Item-Input');
+
+    const newTd2 = document.createElement('td');
+
+    const newdiv2 = document.createElement('div');
+    newdiv2.classList.add('Form-Item');
+
+    const newp2 = document.createElement('p');
+    newp2.classList.add('Form-Item-Label');
+    newp2.textContent = '個数：';
+
+    const newspan2 = document.createElement('span');
+    newspan2.classList.add('Form-Item-Label-Required2');
+    newspan2.textContent = '任意';
+
+    const newinput2 = document.createElement('input');
+    newinput2.type = 'text';
+    newinput2.classList.add('Form-Item-Input');
+
+    const newTdClose = document.createElement('td');
+    newTdClose.classList.add('close-icon','clear-column');
+    newTdClose.textContent = '✖';
+
+    newTr.appendChild(newTd1);
+    newTd1.appendChild(newdiv1);
+    newdiv1.appendChild(newp1);
+    newp1.appendChild(newspan1);
+    newdiv1.appendChild(newinput1);
+
+    newTr.appendChild(newTd2);
+    newTd2.appendChild(newdiv2);
+    newdiv2.appendChild(newp2);
+    newp2.appendChild(newspan2);
+    newdiv2.appendChild(newinput2);
+
+    newTr.appendChild(newTdClose);
+    
+
+  // 「✖」をクリックしたときの処理を追加
+    newTdClose.addEventListener('click', () => {
+        newTr.remove();
+    });
+
+    return newTr;
+}
+
+// 「✖」をクリックしたときの処理
+for (let j = 0; j < closeIcons.length; j++) {
+    closeIcons[j].addEventListener('click', () => {
+        items[j].remove();
+    });
+}
+
+// ボタンをクリックしたときの処理
+btn.addEventListener('click', () => {
+    tbdy.appendChild(createNewTr());
+});
